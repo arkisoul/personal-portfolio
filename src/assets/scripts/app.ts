e58d0b417c558eb6ba5642cf23c97856f4be190e
Arpit <arpit_jain@live.com>
2019-09-14 15:36:56 +0530

    initial commit

---
+++ src/assets/scripts/app.ts
@@ -0,0 +1,10 @@
+// const projectSwiper = new Swiper('.arprojects-swiper', {
+//     slidesPerView: 2,
+//     spaceBetween: 16,
+//     autoplay: true,
+//     loop: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//     },
+// });
